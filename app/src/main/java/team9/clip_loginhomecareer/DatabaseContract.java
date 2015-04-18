db753c184d5f81e0035674571510acfc849807b0
Mary e. Kingsbury <mary.kingsbury@mavs.uta.edu>
2015-04-18 13:35:59 -0500

    added degree data items

--- app/src/main/java/team9/clip_loginhomecareer/DatabaseContract.java
+++ app/src/main/java/team9/clip_loginhomecareer/DatabaseContract.java
@@ -174,12 +174,13 @@ public final class DatabaseContract {
 	private static abstract class EduEntries implements BaseColumns {
 	    public static final String TABLE_NAME = "Colleges";
 	    public static final String _ID = "ID";
-	    public static final String COLUMN_INSTITUTION = "Institution Name";
-        public static final String COLUMN_COLLEGE_CITY = "Institution City";
-        public static final String COLUMN_STUDY_FIELD = "Field of Study";
-        public static final String COLUMN_COMPLETION_DATE = "Graduation Date";
+	    public static final String COLUMN_INSTITUTION = "InstitutionName";
+        public static final String COLUMN_COLLEGE_CITY = "InstitutionCity";
+        public static final String COLUMN_STUDY_FIELD = "FieldStudy";
+        public static final String COLUMN_COMPLETION_DATE = "GraduationDate";
+        public static final String COLUMN_START_DATE = "StartDate";
         public static final String[] ALL_COLUMNS =
-				{_ID, COLUMN_INSTITUTION, COLUMN_COLLEGE_CITY, COLUMN_STUDY_FIELD, COLUMN_COMPLETION_DATE};
+				{_ID, COLUMN_INSTITUTION, COLUMN_COLLEGE_CITY, COLUMN_STUDY_FIELD, COLUMN_COMPLETION_DATE,COLUMN_START_DATE};
 	}
 	// TODO: Place your fields here!
 	// + KEY{...} + " {type} not null"
