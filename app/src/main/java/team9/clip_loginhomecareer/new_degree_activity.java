db47ab487ed4f9bc47cd6271876e807142c2d7ff
Mary e. Kingsbury <mary.kingsbury@mavs.uta.edu>
2015-04-18 14:55:32 -0500

    Merge remote-tracking branch 'origin/FixingBranch' into FixingBranch
    Conflicts:
    	app/src/main/java/team9/clip_loginhomecareer/DatabaseContract.java

--- app/src/main/java/team9/clip_loginhomecareer/new_degree_activity.java
+++ app/src/main/java/team9/clip_loginhomecareer/new_degree_activity.java
@@ -4,16 +4,36 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class new_degree_activity extends ActionBarActivity {
 
+    private DatabaseContract db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        openDB();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_degree_activity);
+
     }
 
+    @Override
+    protected void onDestroy() {
+        closeDB();
+    }
+
+    public void openDB() {
+        db = new DatabaseContract(this);
+        db.open();
+    }
+
+    public void closeDB() {
+        db.close();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +55,51 @@ public class new_degree_activity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void add_new(View v) {
+        EditText text;
+        if (validItems()) {
+            text = (EditText) findViewById(R.id.new_college_name);
+            String inst = text.getText().toString();
+            text = (EditText) findViewById(R.id.new_college_location);
+            String loc = text.getText().toString();
+            text = (EditText) findViewById(R.id.new_college_degree_sought);
+            String ds = text.getText().toString();
+            text = (EditText) findViewById(R.id.new_college_field_of_study);
+            String major = text.getText().toString();
+            text = (EditText) findViewById(R.id.new_college_grad_date);
+            Integer grad = Integer.parseInt(text.getText().toString());
+            text = (EditText) findViewById(R.id.new_college_start_date);
+            Integer matr = Integer.parseInt(text.getText().toString());
+
+            db.insertColleges(inst, loc, major, ds, matr, grad, 0);
+            toastNotification("Degree Saved");
+            clearData();
+        } else {
+            toastNotification("Invalid Information");
+        }
+    }
+    private boolean validItems() {
+        //TODO: Check for invalid input data
+        return true;
+    }
+    private void toastNotification(String description) {
+        Toast.makeText(getApplicationContext(), description, Toast.LENGTH_LONG).show();
+    }
+    private void clearData() {
+        EditText text;
+        text = (EditText) findViewById(R.id.new_college_name);
+        text.setText("");
+        text = (EditText) findViewById(R.id.new_college_degree_sought);
+        text.setText("");
+        text = (EditText) findViewById(R.id.new_college_location);
+        text.setText("");
+        text = (EditText) findViewById(R.id.new_college_start_date);
+        text.setText("");
+        text = (EditText) findViewById(R.id.new_college_grad_date);
+        text.setText("");
+        text = (EditText) findViewById(R.id.new_college_field_of_study);
+        text.setText("");
     }
 }
