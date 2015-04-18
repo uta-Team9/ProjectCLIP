b8cb5c976f2351ee5118f9386e9e55b896d1852c
Mary e. Kingsbury <mary.kingsbury@mavs.uta.edu>
2015-04-18 17:45:28 -0500

    degrees should list now

--- app/src/main/java/team9/clip_loginhomecareer/DegreesList.java
+++ app/src/main/java/team9/clip_loginhomecareer/DegreesList.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -15,6 +19,7 @@ public class DegreesList extends ActionBarActivity {
     private int User_ID = 0;
     private DatabaseContract db;
     private ArrayList<Degree> degreeArrayList = new ArrayList<>();
+    private ListView degrees;
 
     public ArrayList<Degree> getDegreeArrayList() {
         return degreeArrayList;
@@ -26,12 +31,38 @@ public class DegreesList extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        openDB();
+        Bundle extras = getIntent().getExtras();
+        if(extras != null)
+        {
+            User_ID = extras.getInt("ID");
+            Log.d("User ID: ", "" + User_ID);
+        }
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.education_degrees_list);
+
+        degrees = (ListView) findViewById(R.id.degree_list);
+
+        Button edit = (Button) findViewById(R.id.degree_list);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewInstance(v);
+            }
+        });
+
+        buildList();
+
+		/*ArrayList<String> temp = new ArrayList<>();
+		for(Contact c : list) {
+			temp.add(c.toString());
+		}*/
+
+        ArrayAdapter<Degree> arrayAdapter = new ArrayAdapter<Degree>(
+                this, android.R.layout.simple_list_item_1, degreeArrayList);
+
+        degrees.setAdapter(arrayAdapter);
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -76,6 +107,14 @@ public class DegreesList extends ActionBarActivity {
         }
         cursor.close();
 
+    }
+
+    private void openDB() {
+        db = new DatabaseContract(this);
+        db.open();
+    }
+    private void closeDB() {
+        db.close();
     }
 
 
