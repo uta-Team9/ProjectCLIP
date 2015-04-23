954580745e5710b09a89a96b9bd1da29a5ba1aa8
ajoysen <ajoysen@gmail.com>
2015-04-23 11:48:01 -0500

    All completed forms

--- app/src/main/java/team9/clip_loginhomecareer/FinanceLiabilityNew.java
+++ app/src/main/java/team9/clip_loginhomecareer/FinanceLiabilityNew.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class FinanceLiabilityNew extends ActionBarActivity {
+
+    private DatabaseContract db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.finance_liability_new);
+        db = new DatabaseContract(this);
+        db.open();
     }
 
 
@@ -38,9 +44,37 @@ public class FinanceLiabilityNew extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void add_new(View v) {
+        EditText text;
+        if(validItems()) {
+            text = (EditText) findViewById(R.id.txt_cash_source);
+            String source = text.getText().toString();
+            text = (EditText) findViewById(R.id.txt_cash_amt);
+            Double amount = Double.parseDouble(text.getText().toString());
+            text = (EditText) findViewById(R.id.txt_cash_note);
+            String note = text.getText().toString();
+
+            //TODO: get date and userID for saving
+            db.insertCash(amount, source, note, 2015, 0);
+            //Log.d("Contact Saved: ", "" + source);
+            toastNotification("Contact Saved");
+            //clearData();
+        } else {
+            toastNotification("Invalid Information");
+        }
+
+    }
     public void cancel(View v) {
         Intent intent = null;
         intent = new Intent(this, FinanceLiabilityView.class);
         startActivity(intent);
+    }
+    private boolean validItems() {
+        //TODO: Check for invalid input data
+        return true;
+    }
+
+    private void toastNotification(String description) {
+        Toast.makeText(getApplicationContext(), description, Toast.LENGTH_LONG).show();
     }
 }
