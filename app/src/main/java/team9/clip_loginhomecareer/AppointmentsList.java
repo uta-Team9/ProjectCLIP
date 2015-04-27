efb782f27457e6b703c5bd3e54efc1474703f49a
Ameera Khan <ameera.khan@mavs.uta.edu>
2015-04-26 19:23:21 -0500

    Health Update. Problems Fixed.

--- app/src/main/java/team9/clip_loginhomecareer/AppointmentsList.java
+++ app/src/main/java/team9/clip_loginhomecareer/AppointmentsList.java
@@ -42,8 +42,6 @@ public class AppointmentsList extends ActionBarActivity
         super.onDestroy();
     }
 
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
