716cde62da7d010df446d2a522835329a05dcbe3
Mary e. Kingsbury <mary.kingsbury@mavs.uta.edu>
2015-05-04 01:25:25 -0500

    college application work

--- app/src/main/java/team9/clip_loginhomecareer/ViewAppl.java
+++ app/src/main/java/team9/clip_loginhomecareer/ViewAppl.java
@@ -1,17 +1,35 @@
 package team9.clip_loginhomecareer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ViewAppl extends ActionBarActivity {
+    private int User_ID;
+    private EduApp CollApp = null;
+    private DatabaseContract db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        openDB();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_appl);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            db = new DatabaseContract(this);
+            db.open();
+            CollApp = (EduApp) extras.getSerializable("College Application");
+
+            setTitle(CollApp.getCollege());
+            setUpTextBoxes();
+        }
     }
 
 
@@ -35,5 +53,35 @@ public class ViewAppl extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void openDB() {
+        db = new DatabaseContract(this);
+        db.open();
+        User_ID = getSharedPreferences("loginPrefs", MODE_PRIVATE).getInt("ID", -1);
+        Log.d("ID in ViewAppl", "" + User_ID);
+    }
+    private void setUpTextBoxes() {
+        if(CollApp != null) {
+            TextView text = (TextView) findViewById(R.id.edu_detail_collApp);
+            text.setText("" + CollApp.getCollege());
+            text = (TextView) findViewById(R.id.edu_appl_reply);
+            text.setText("" + CollApp.getReply_expected());
+            text = (TextView) findViewById(R.id.edu_appl_due);
+            text.setText("" + CollApp.getDeadline());
+
+        }
+    }
+    public void editInstance(View v) {
+        startActivity(new Intent(this, EduNewApp.class).putExtra("College Application", CollApp));
+    }
+    private void toast(String description) {
+        Toast.makeText(getApplicationContext(), description, Toast.LENGTH_LONG).show();
+    }
+    public void deleteInstance(View v) {
+        if(db.deleteCollege(CollApp.getDbRow())) {
+            toast("Application removed");
+            finish();
+        } else
+            toast("Application already removed");
     }
 }
